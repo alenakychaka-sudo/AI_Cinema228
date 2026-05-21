b5e1b9019956224e6c38ab3681126ec53d31f0e1
Atrhur <alenakychaka@gmail.com>
2026-05-21 23:53:48 +0300

    wast connected library OpenAI and added system prompt, added history dialog for AI

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+import json
+from openai import OpenAI
+
 class Message(BaseModel):
     question: str
     answer: str
@@ -8,7 +11,6 @@ class BotRequest(BaseModel):
     user_id: int
     consversion: list[Message]
 
-app = FastAPI()
 app = FastAPI()
 
 @app.get("/ping")
@@ -19,6 +21,12 @@ async def ping_server():
 @app.post('/chat')
 async def process_chat(request: BotRequest):
     histori_lenght = len(request.consversion)
+    massege_for_ai = [
+        {"role": "system", "content": "Ты бот Кинотавр. Узнай настроение пользователя и порекомендуй фильм."}
+    ]
+    for msg in request.consversion:
+        massege_for_ai.append({"role": "assistant", "content": msg.question})
+        massege_for_ai.append({"role": "user", "content": msg.answer})
     return {
         "status": "success",
         "message": f"Юзер {request.user_id} прислал {histori_lenght} сообщений"
